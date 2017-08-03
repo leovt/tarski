c6d3335145518bb825fe1a5fec5acfdf41966e76
leovt <leonhard.vogt@gmx.ch>
2017-08-03 22:46:25 +0200

    Following the order of Axioms and Theorems in the book.

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from proof import ProofContext
 from tarski import axioms, Congruent, Equal, Between
-
+from formula import (UniversalQuantifier as ForAll,
+                     ExistentialQuantifier as Exists)
 
 p = ProofContext(axioms)
 x,y = p.start_context(2)
-F1 = p.specialise(axioms[1], (x,y,y,x,y,x))
-F2 = p.specialise(axioms[0], (x,y))
+F1 = p.specialise(axioms[1], (y,x,x,y,x,y))
+F2 = p.specialise(axioms[0], (y,x))
 p.conjunction(F2, F2)
 F3 = p.modus_ponens(F1)
-Thm1 = p.directproof(F3)
+Thm_2_1 = p.directproof(F3)
+assert Thm_2_1 == ForAll((1,2), Congruent(1,2,1,2))
 
 x,y,r,s = p.start_context(4)
 F3 = p.assume(Congruent(x,y,r,s))
-F4 = p.specialise(Thm1, (y,x))
+F4 = p.specialise(Thm_2_1, (x,y))
 F5 = p.specialise(axioms[1], (x,y,r,s,x,y))
 F6 = p.conjunction(F3, F4)
 F7 = p.modus_ponens(F5)
-Thm2 = p.directproof(Congruent(r,s,x,y))
+Thm_2_2 = p.directproof(Congruent(r,s,x,y))
+assert Thm_2_2 == ForAll((1,2,3,4), Congruent(1,2,3,4) > Congruent(3,4,1,2))
+
--- tarski.py
+++ tarski.py
@@ -8,18 +8,49 @@ Congruent = Predicate('Congruent', 4, '{}{}~{}{}')
 Between = Predicate('Between', 3, '({1} in {0}{2})')
 
 x,y,r,s,t,u = Term(), Term(), Term(), Term(), Term(), Term()
-str(x),str(y),str(r),str(s),str(t),str(u)
-
+x1,y1,r1,s1 = 1,2,3,4
 axioms = [
     ForAll((x,y), Congruent(x,y,y,x)),
     ForAll((x,y,r,s,t,u), Congruent(x,y,r,s) & Congruent(x,y,t,u) > Congruent(r,s,t,u)),
+
+    # Identitätsaxiom für die Streckenkongruenz
     ForAll((x,y,r), Congruent(x,y,r,r) > Equal(x,y)),
 
+    # Axiom der Streckenabtragung
+    ForAll((y,r,s,t), Exists((x,), Between(y,r,x) & Congruent(r,x,s,t))),
+    
+    # Fünf-Strecken-Axiom
+    ForAll((x,y,r,s,x1,y1,r1,s1),
+           -Equal(x,y) &
+           Between(x,y,r) & Between(x1,y1,r1) &
+           Congruent(x,y,x1,y1) &
+           Congruent(y,r,y1,r1) &
+           Congruent(x,s,x1,s1) &
+           Congruent(y,s,y1,s1) 
+           > Congruent(r,s,r1,s1)),
+
+    # Identitätsaxiom für die Zwischenbeziehung
     ForAll((x,y), Between(x,y,x) > Equal(x,y)),
+    
+    # Axiom von Pasch, innere Form
     ForAll((x,y,r,s,t), Between(x,r,t) & Between(x,s,t) > Exists((u,), Between(r,u,y) & Between(s,u,x))),
-    #continuity missing
+
+    # unteres Dimensionsaxiom
     Exists((r,s,t), -Between(r,s,t) & -Between(s,t,r) & -Between(t,r,s)),
-    ForAll((x,y,r,s,t), Congruent(r,x,r,y) & Congruent(s,x,s,y) & Congruent(t,x,t,y) > Between(r,s,t) | Between(s,t,r) | Between(t,r,s)),
+    
+    # oberes Dimensionsaxiom
+    ForAll((x,y,r,s,t), 
+           -Equal(x,y) &
+           Congruent(r,x,r,y) & Congruent(s,x,s,y) & Congruent(t,x,t,y) 
+           > Between(r,s,t) | Between(s,t,r) | Between(t,r,s)),
+          
+    # Euklidisches Axiom
+    ForAll((r,s,t,u,x),
+           -Equal(r,u) &
+           Between(r,u,x) & Between(s,u,t) 
+           > Exists((x1,y1), Between(r,s,x1) & Between(r,t,y1) & Between(x1,x,y1))),
+    
+    # Stetigkeit
     ]
 
 
