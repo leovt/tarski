4ca0a62fdf9c735cc1d5e77c5d7aa67fa7a22ba3
leovt <leonhard.vogt@gmx.ch>
2017-08-08 22:47:59 +0200

    inference rules for equality inference rules for conjunctions inference rules for existential quantifier

--- proof.py
+++ proof.py
@@ -1,5 +1,6 @@
-from formula import Term
+from formula import Term, ExistentialQuantifier
 from itertools import count
+from tarski import Equal
 
 class DictStack(dict):
     def __init__(self, parent=None):
@@ -68,7 +69,7 @@ class ProofContext():
             for x in assump[1:]:
                 conditions = conditions & x
             new_fact = conditions > fact
-        new_fact = new_fact.generalize(freevars)
+        new_fact = new_fact.generalize([v for v in freevars if v in new_fact.free()])
         evidence = dict(self.facts)
         self.facts = self.facts.pop()
         self._add(new_fact, 'direct proof', (), evidence)
@@ -80,12 +81,44 @@ class ProofContext():
             new_fact = new_fact.specialise(i, s)
         self._add(new_fact, 'universal specialisation', [fact])
         return new_fact
+    
+    def instantiate(self, fact):
+        assert isinstance(fact, ExistentialQuantifier)
+        subs = [(var,Term()) for var in fact.bound()]
+        new_vars = [t for _,t in subs]
+        self.freevars[-1].extend(new_vars)
+        subs = dict(subs)
+        new_fact = fact.formula.substitute(subs)
+        self._add(new_fact, 'existential instantiation', [fact])
+        return new_vars, new_fact
 
     def conjunction(self, left, right):
         new_fact = left & right
         self._add(new_fact, 'conjunction', (left, right))
         return new_fact
+    
+    def deduce_left(self, fact):
+        assert fact.connective == '&'
+        new_fact = fact.left
+        self._add(new_fact, 'left part of conjunction', [fact])
+        return new_fact
 
+    def deduce_right(self, fact):
+        assert fact.connective == '&'
+        new_fact = fact.right
+        self._add(new_fact, 'right part of conjunction', [fact])
+        return new_fact
+    
+    def substitute_equal(self, formula, new_formula, identity):
+        assert identity.predicate == Equal
+        x,y = identity.terms
+        z = Term()
+        test1 = new_formula.substitute({x:z, y:z})
+        test2 = formula.substitute({x:z, y:z})
+        assert test1 == test2, (str(test1), str(test2))
+        self._add(new_formula, 'equality substitution', [formula, identity])
+        return new_formula
+        
     def modus_ponens(self, implication):
         assert implication.connective == '->'
         self._add(implication.right, 'modus ponens', (implication, implication.left))
