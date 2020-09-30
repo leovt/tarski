4072b7ed906aa4f002ca9e317e06b8430513baa9
leovt <leonhard.vogt@gmx.ch>
2020-09-30 22:18:52 +0200

    more proofs

--- main.py
+++ main.py
@@ -121,63 +121,198 @@ assert axioms[4] == ForAll((11,12,13,14,21,22,23,24),
                            AFS(11,12,13,14,21,22,23,24)
                            > Congruent(13,14,23,24))
 
-a,b,c,a1,b1,c1 = p.start_context(6)
-B1 = p.assume(Between(a,b,c) & Between(a1,b1,c1))
-B2 = p.assume(Congruent(a,b,a1,b1))
-B3 = p.assume(Congruent(b,c,b1,c1))
-B4 = p.specialise(Thm_2_8, (a,a1))
-B5 = p.modus_ponens(p.specialise(Thm_2_5_bis, (a,b,a1,b1)))
-B6 = p.conjunction(p.conjunction(p.conjunction(p.conjunction(B1,B2),B3),B4),B5)
-assert B6 == AFS(a,b,c,a,a1,b1,c1,a1)
-p.start_context(0)
-C1 = p.assume(Equal(a,b))
-C2 = p.substitute_equal(B2, Congruent(a,a,a1,b1), C1)
-C3 = p.modus_ponens(p.specialise(Thm_2_2, (a,a,a1,b1)))
-C4 = p.modus_ponens(p.specialise(axioms[2], (a1,b1,a)))
-C5 = p.substitute_equal(B3, Congruent(a,c,b1,c1), C1)
-C6 = p.substitute_equal(C5, Congruent(a,c,a1,c1), C4)
-assert C6 == Congruent(a,c,a1,c1)
-B7 = p.directproof(C6)
-p.start_context(0)
-D1 = p.assume(-Equal(a,b))
-D2 = p.conjunction(D1, B6)
-D3 = p.modus_ponens(p.specialise(axioms[4], (a,b,c,a,a1,b1,c1,a1)))
-D4 = p.modus_ponens(p.specialise(Thm_2_5_bis, (c,a,c1,a1)))
-assert D4 == Congruent(a,c,a1,c1)
-B8 = p.directproof(D4)
-p.tertium_non_datur(Equal(a,b))
-B9 = p.disjunction_elimination(Equal(a,b), -Equal(a,b), Congruent(a,c,a1,c1))
-Thm_2_11 = p.directproof(B9)
-assert Thm_2_11 == ForAll((1,2,3,4,5,6),
-                          Between(1,2,3) & Between(4,5,6) &
-                          Congruent(1,2,4,5) & Congruent(2,3,5,6)
-                          > Congruent(1,3,4,6))
+@theorem(ForAll((1,2,3,4,5,6),
+         Between(1,2,3) & Between(4,5,6) &
+         Congruent(1,2,4,5) & Congruent(2,3,5,6)
+         > Congruent(1,3,4,6)))
+def Thm_2_11():
+    """adding line segments
 
-a,b = p.start_context(2)
-A1 = p.specialise(axioms[3], (a,b,b,b))
-(x,),A2 = p.instantiate(A1)
-A3 = p.deduce_left(A2)
-A4 = p.deduce_right(A2)
-A5 = p.modus_ponens(p.specialise(axioms[2], (b,x,b)))
-A6 = p.substitute_equal(A3, Between(a,b,b), A5)
-Thm_3_1 = p.directproof(A6)
-assert Thm_3_1 == ForAll((1,2), Between(1,2,2))
+    1--2===3   4--5===6    =>   1~~3  ==  4~~6
+    """
+    a,b,c,a1,b1,c1 = p.start_context(6)
+    B1 = p.assume(Between(a,b,c) & Between(a1,b1,c1))
+    B2 = p.assume(Congruent(a,b,a1,b1))
+    B3 = p.assume(Congruent(b,c,b1,c1))
+    B4 = p.specialise(Thm_2_8, (a,a1))
+    B5 = p.modus_ponens(p.specialise(Thm_2_5_bis, (a,b,a1,b1)))
+    B6 = p.conjunction(p.conjunction(p.conjunction(p.conjunction(B1,B2),B3),B4),B5)
+    assert B6 == AFS(a,b,c,a,a1,b1,c1,a1)
+    p.start_context(0)
+    C1 = p.assume(Equal(a,b))
+    C2 = p.substitute_equal(B2, Congruent(a,a,a1,b1), C1)
+    C3 = p.modus_ponens(p.specialise(Thm_2_2, (a,a,a1,b1)))
+    C4 = p.modus_ponens(p.specialise(axioms[2], (a1,b1,a)))
+    C5 = p.substitute_equal(B3, Congruent(a,c,b1,c1), C1)
+    C6 = p.substitute_equal(C5, Congruent(a,c,a1,c1), C4)
+    assert C6 == Congruent(a,c,a1,c1)
+    B7 = p.directproof(C6)
+    p.start_context(0)
+    D1 = p.assume(-Equal(a,b))
+    D2 = p.conjunction(D1, B6)
+    D3 = p.modus_ponens(p.specialise(axioms[4], (a,b,c,a,a1,b1,c1,a1)))
+    D4 = p.modus_ponens(p.specialise(Thm_2_5_bis, (c,a,c1,a1)))
+    assert D4 == Congruent(a,c,a1,c1)
+    B8 = p.directproof(D4)
+    p.tertium_non_datur(Equal(a,b))
+    B9 = p.disjunction_elimination(Equal(a,b), -Equal(a,b), Congruent(a,c,a1,c1))
+    return p.directproof(B9)
 
-a,b,c = p.start_context(3)
-p.conjunction(
-    p.assume(Between(a,b,c)),
-    p.specialise(Thm_3_1, (b,c)))
-A1=p.modus_ponens(p.specialise(axioms[6], (a,b,b,c,c)))
-(x,),A2=p.instantiate(A1)
-p.deduce_left(A2)
-A3 = p.modus_ponens(p.specialise(axioms[5], (b,x)))
-A4 = p.substitute_equal(p.deduce_right(A2), Between(c,b,a), A3)
-Thm_3_2 = p.directproof(A4)
-assert Thm_3_2 == ForAll((1,2,3), Between(1,2,3) > Between(3,2,1))
+@theorem(ForAll((1,2,3,4,5,6),
+    -Equal(1,2) &
+    Between(1,2,5) & Congruent(2,5,3,4) &
+    Between(1,2,6) & Congruent(2,6,3,4) > Equal(5,6)))
+def Thm_2_12():
+    """uniqueness of adding line segments"""
+    #ForAll((y,r,s,t), Exists((x,), Between(y,r,x) & Congruent(r,x,s,t)))
+    q,a,b,c,x,y = p.start_context_names('qabcxy')
+    A0 = p.assume(-Equal(q,a))
+    A1x = p.assume(Between(q,a,x))
+    A2x = p.assume(Congruent(a,x,b,c))
+    A1y = p.assume(Between(q,a,y))
+    A2y = p.assume(Congruent(a,y,b,c))
+    Cqaqa = p.specialise(Thm_2_1, (q,a))
+    p.conjunction(
+        A2x,
+        p.modus_ponens(p.specialise(Thm_2_2, (a,y,b,c))),
+    )
+    p.modus_ponens(p.specialise(Thm_2_3, (a,x,b,c,a,y)))
+    p.conjunction(p.conjunction(p.conjunction(A1x, A1y), Cqaqa), Congruent(a,x,a,y))
+    p.modus_ponens(p.specialise(Thm_2_11, (q,a,x,q,a,y)))
+    print(axioms[4])
+    p.conjunction(
+        A0,
+        p.conjunction(
+        p.conjunction(
+        p.conjunction(
+            p.conjunction(
+                p.conjunction(A1x, A1x),
+                Cqaqa,
+            ),
+            p.specialise(Thm_2_1, (a,x))
+            ),
+            Congruent(q,x,q,y),
+            ),
+            Congruent(a,x,a,y))
+        )
+    p.modus_ponens(p.specialise(axioms[4], (q,a,x,x, q,a,x,y)))
+    p.modus_ponens(p.specialise(Thm_2_2, (x,x,x,y)))
+    p.modus_ponens(p.specialise(axioms[2], (x,y,x)))
+    return p.directproof(Equal(x,y))
 
-a,b = p.start_context_names('ab')
-p.specialise(Thm_3_1, (b,a))
-p.specialise(Thm_3_2, (b,a,a))
-p.modus_ponens2(Between(b,a,a), Between(a,a,b))
-Thm_3_3 = p.directproof(Between(a,a,b))
-assert Thm_3_3 == ForAll((1,2), Between(1,1,2))
+@theorem(ForAll((1,2), Between(1,2,2)))
+def Thm_3_1():
+    """right endpoint is between start and end"""
+    a,b = p.start_context(2)
+    A1 = p.specialise(axioms[3], (a,b,b,b))
+    (x,),A2 = p.instantiate(A1)
+    A3 = p.deduce_left(A2)
+    A4 = p.deduce_right(A2)
+    A5 = p.modus_ponens(p.specialise(axioms[2], (b,x,b)))
+    A6 = p.substitute_equal(A3, Between(a,b,b), A5)
+    return p.directproof(A6)
+
+@theorem(ForAll((1,2,3), Between(1,2,3) > Between(3,2,1)))
+def Thm_3_2():
+    """between is symmetric in the endpoints"""
+    a,b,c = p.start_context(3)
+    p.conjunction(
+        p.assume(Between(a,b,c)),
+        p.specialise(Thm_3_1, (b,c)))
+    A1=p.modus_ponens(p.specialise(axioms[6], (a,b,b,c,c)))
+    (x,),A2=p.instantiate(A1)
+    p.deduce_left(A2)
+    A3 = p.modus_ponens(p.specialise(axioms[5], (b,x)))
+    A4 = p.substitute_equal(p.deduce_right(A2), Between(c,b,a), A3)
+    return p.directproof(A4)
+
+@theorem(ForAll((1,2), Between(1,1,2)))
+def Thm_3_3():
+    """left endpoint is between start and end"""
+    a,b = p.start_context_names('ab')
+    p.specialise(Thm_3_1, (b,a))
+    p.specialise(Thm_3_2, (b,a,a))
+    p.modus_ponens2(Between(b,a,a), Between(a,a,b))
+    return p.directproof(Between(a,a,b))
+    assert Thm_3_3 == ForAll((1,2), Between(1,1,2))
+
+
+@theorem(ForAll((1,2,3), Between(1,2,3) & Between(2,1,3) > Equal(1,2)))
+def Thm_3_4():
+    """squeeze point
+
+    1-2----3  &&  2-1----3  ==>  1=2
+    """
+    a,b,c = p.start_context_names('abc')
+    p.assume(Between(a,b,c) & Between(b,a,c))
+    A7 = p.specialise(axioms[6], (a,b,b,a,c))
+    Ex = p.modus_ponens(A7)
+    (x,), Px = p.instantiate(Ex)
+    # wir erhalten Punkt x mit Bbxb und Baxa
+    p.deduce_left(Px)
+    p.deduce_right(Px)
+    A61 = p.modus_ponens(p.specialise(axioms[5], (a,x)))
+    A62 = p.modus_ponens(p.specialise(axioms[5], (b,x)))
+    Eq = p.substitute_equal(A61, Equal(a,b), A62)
+    return p.directproof(Eq)
+
+@theorem(ForAll((1,2,3,4), Between(1,2,4) & Between(2,3,4) >
+                           Between(1,2,3)))
+def Thm_3_5_1():
+    """
+     1--2-----4
+        2--3--4
+    ============
+     1--2--3
+    """
+    a,b,c,d = p.start_context_names('abcd')
+    p.assume(Between(a,b,d) & Between(b,c,d))
+    (x,), BbxbBcxa = p.instantiate(
+        p.modus_ponens(
+            p.specialise(axioms[6], (a,b,b,c,d))))
+    p.deduce_left(BbxbBcxa)
+    x_eq_b = p.modus_ponens(p.specialise(axioms[5], (b,x)))
+    p.substitute_equal(p.deduce_right(BbxbBcxa), Between(c,b,a), x_eq_b)
+    return p.directproof(p.modus_ponens(p.specialise(Thm_3_2, (c,b,a))))
+
+@theorem(ForAll((1,2,3,4), Between(1,2,3) & Between(1,3,4) >
+                           Between(2,3,4)))
+def Thm_3_6_1():
+    """"""
+    a,b,c,d = p.start_context_names('abcd')
+    p.assume(Between(a,b,c))
+    p.assume(Between(a,c,d))
+    p.conjunction(
+        p.modus_ponens(p.specialise(Thm_3_2, (a,c,d))),
+        p.modus_ponens(p.specialise(Thm_3_2, (a,b,c))))
+    p.modus_ponens(p.specialise(Thm_3_5_1, (d,c,b,a)))
+    return p.directproof(p.modus_ponens(p.specialise(Thm_3_2, (d,c,b))))
+
+@theorem(ForAll((1,2,3,4), Between(1,2,3) & Between(2,3,4) & -Equal(2,3) > Between(1,3,4)))
+def Thm_3_7_1():
+    """"""
+    a,b,c,d = p.start_context_names('abcd')
+    Babc = p.assume(Between(a,b,c))
+    Bbcd = p.assume(Between(b,c,d))
+    b_neq_c = p.assume(-Equal(b,c))
+
+    (x,), BC = p.instantiate(p.specialise(axioms[3], (a,c,c,d)), 'x')
+    Bacx = p.deduce_left(BC)
+    p.conjunction(Babc, Bacx)
+    Bbcx = p.modus_ponens(p.specialise(Thm_3_6_1, (a,b,c,x)))
+    p.conjunction(
+        p.conjunction(
+            p.conjunction(
+                p.conjunction(b_neq_c,Bbcx),
+                p.deduce_right(BC)),
+            Bbcd),
+        p.specialise(Thm_2_1, (c,d)))
+    x_eq_d = p.modus_ponens(p.specialise(Thm_2_12, (b,c,c,d,x,d)))
+    p.substitute_equal(Bacx, Between(a,c,d), x_eq_d)
+    return p.directproof(Between(a,c,d))
+
+
+# @theorem(ForAll((1,2,3,4,5,6),
+#     -Equal(1,2) &
+#     Between(1,2,5) & Congruent(2,5,3,4) &
+#     Between(1,2,6) & Congruent(2,6,3,4) > Equal(5,6)))
